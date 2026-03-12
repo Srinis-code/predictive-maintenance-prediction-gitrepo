369a22d185d8cf38a737eabce30670404a5ac223
Srinis-code <ksricheenu@gmail.com>
2026-03-12 16:31:36 +0530

    Update hosting.py

--- predictive-maintenance/hosting/hosting.py
+++ predictive-maintenance/hosting/hosting.py
@@ -4,8 +4,8 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 
 api.upload_folder(
-    folder_path="predictive-maintenance/deployment",     # the local folder containing your files
-    repo_id="ksricheenu/Predictive-Maintenance-Prediction",          # the target repo
+    folder_path="predictive-maintenance/hosting",        # the local folder containing your files
+    repo_id="ksricheenu/Predictive-Maintenance-Prediction", # the target repo space in huggingface
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
